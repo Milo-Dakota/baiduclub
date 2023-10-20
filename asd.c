67784450958fc457d915b1807b9fc0d4991c0b8b
plafle <passenger2287923518@gmail.com>
2023-10-20 21:38:25 +0800

    commit

---
+++ asd.c
@@ -0,0 +1,15 @@
+ /********** 使用short变量k和p，在下面写出满足任务要求的表达式**********/
+ /**********Begin**********/
+#include<stdio.h>
+
+short k,p;
+int main() {
+   scanf("%d%d",&k,&p);
+    
+    short c = (k >> 8) << 8;
+    
+    short d = p >> 8;
+    printf("%d\n",c + d);
+    return 0;
+}
+ /**********End**********/
